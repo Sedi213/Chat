ae06a87a46655b1fa0e0cc04ac646948ff760b81
Kovtun Kostia <sedmaster213@gmail.com>
2022-07-06 11:35:22 +0300

    Pun Ball

--- Tenis/Form1.Designer.cs
+++ Tenis/Form1.Designer.cs
@@ -28,47 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.components = new System.ComponentModel.Container();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.Player1 = new System.Windows.Forms.PictureBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.restartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // pictureBox3
+            // Ball
             // 
-            this.pictureBox3.Image = global::Tenis.Properties.Resources._11;
-            this.pictureBox3.Location = new System.Drawing.Point(355, 194);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 2;
-            this.pictureBox3.TabStop = false;
+            this.Ball.Image = global::Tenis.Properties.Resources._11;
+            this.Ball.Location = new System.Drawing.Point(355, 194);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(39, 36);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ball.TabIndex = 2;
+            this.Ball.TabStop = false;
             // 
-            // pictureBox2
+            // Player2
             // 
-            this.pictureBox2.Image = global::Tenis.Properties.Resources._3;
-            this.pictureBox2.Location = new System.Drawing.Point(772, 48);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(16, 95);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.Player2.Image = global::Tenis.Properties.Resources._3;
+            this.Player2.Location = new System.Drawing.Point(772, 194);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(16, 95);
+            this.Player2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player2.TabIndex = 1;
+            this.Player2.TabStop = false;
             // 
-            // pictureBox1
+            // Player1
             // 
-            this.pictureBox1.Image = global::Tenis.Properties.Resources._2;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 48);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(16, 95);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.Player1.Image = global::Tenis.Properties.Resources._2;
+            this.Player1.Location = new System.Drawing.Point(12, 194);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(16, 95);
+            this.Player1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Player1.TabIndex = 0;
+            this.Player1.TabStop = false;
             // 
             // menuStrip1
             // 
@@ -94,24 +97,39 @@
             this.restartToolStripMenuItem.Name = "restartToolStripMenuItem";
             this.restartToolStripMenuItem.Size = new System.Drawing.Size(69, 24);
             this.restartToolStripMenuItem.Text = "Restart";
-            this.restartToolStripMenuItem.Click += new System.EventHandler(this.restartToolStripMenuItem_Click);
+            this.restartToolStripMenuItem.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -121,12 +139,14 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox Player1;
+        private System.Windows.Forms.PictureBox Player2;
+        private System.Windows.Forms.PictureBox Ball;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem restartToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Tenis/Form1.cs
+++ Tenis/Form1.cs
@@ -17,20 +17,61 @@ namespace Tenis
             InitializeComponent();
         }
 
-        private int direction;
+        private bool UpMove;
+        private bool LeftMove;
+
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            Random random = new Random();
+            Ball.Location = new Point(random.Next(290,330), random.Next(160, 180));
+            UpMove = random.Next(2) == 0;
+            LeftMove = random.Next(2) == 0;
+            timer1.Enabled = true;
         }
 
         private void settingToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            
         }
 
-        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+   
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
-            direction = (new Random()).Next(-10,10);
+            Ball.Left += LeftMove ? -2 : 2;
+            Ball.Top += UpMove ? -2 : 2;
+            
+            
+            Colizion();
+            CheckLose();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.S && (Player1.Top+ Player1.Height) < 340)
+                Player1.Top += 3;
+            if (e.KeyCode == Keys.W && (Player1.Top) > 30)
+                Player1.Top -= 3;
+            if (e.KeyCode == Keys.Down && (Player2.Top + Player2.Height) < 340)
+                Player2.Top += 3;
+            if (e.KeyCode == Keys.Up && (Player2.Top) > 30)
+                Player2.Top -= 3;
+        }
+
+        private void Colizion() {  
+            if (Ball.Left <= 20 && (Ball.Top+Ball.Height) > Player1.Top && Ball.Top < (Player1.Top+Player1.Height)) LeftMove = !LeftMove;
+            if (Ball.Left >= 555 && (Ball.Top + Ball.Height) > Player2.Top && Ball.Top < (Player2.Top + Player2.Height)) LeftMove = !LeftMove;
+            if (Ball.Top <= 20 || Ball.Top >= 340) UpMove = !UpMove;
+        }
+            private void CheckLose() {
+            if (Ball.Left > 556) {
+                timer1.Enabled = false;
+                MessageBox.Show("Player 2 LOSE");     
+            }
+            if (Ball.Left < 19) {
+                timer1.Enabled = false;
+                MessageBox.Show("Player 1 LOSE");
+            }
         }
     }
 }
